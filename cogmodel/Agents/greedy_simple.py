4f7a63074aa9a2a248b5e3e24d62bee88fa55631
jgiannikos <jgiannikos@techfak.uni-bielefeld.de>
2022-06-19 14:54:23 -0400

    adjusted agent so that no immediate steps are taken. Agent now always moves to the best adjacent field. This leads to less cognitive load and should fix issues with non terminating paths

--- cogmodel/Agents/greedy_simple.py
+++ cogmodel/Agents/greedy_simple.py
@@ -3,9 +3,9 @@ import math
 from cogmodel.gridEnvironment import TURN_RIGHT, TURN_LEFT
 import numpy as np
 
-MAX_STEPS = 1000
-AGENT_AMBITION = 1  # 1 should be the base value here. This means that any field which has been visited less and is an actual improvement distance wise is chosen immediately
-BACKSTEP_PENALTY = 4
+#MAX_STEPS = 1000
+#AGENT_AMBITION = 4 # 1 should be the base value here. This means that any field which has been visited less and is an actual improvement distance wise is chosen immediately
+#BACKSTEP_PENALTY = 4
 
 
 class greedy(object):
@@ -16,7 +16,7 @@ class greedy(object):
     def __init__(self, gridEnvironment):
         self.env = gridEnvironment  # env on which agent runs
         self.visited = {gridEnvironment.agent_pos: 1}
-        self.last_pos = gridEnvironment.agent_pos
+        #self.last_pos = gridEnvironment.agent_pos
         self.options = []
 
     def run(self):
@@ -27,15 +27,13 @@ class greedy(object):
         # log header of logging file
         self.env.start_experiment()
 
-        i = 0
-        while(self.env.agent_pos != self.env.target and i < MAX_STEPS):
+        while(self.env.agent_pos != self.env.target):
             self._choose_action()
-            i += 1
 
-        if i == MAX_STEPS:
-            print("agent death termination")
-        else:
-            print("goal found")
+        #if i == MAX_STEPS:
+        #    print("agent death termination")
+        #else:
+        #    print("goal found")
         # log footer of logging file
         self.env.finish_experiment()
 
@@ -46,6 +44,7 @@ class greedy(object):
             for tile in self.env.get_view_cone():
                 if tile == (self.env.agent_pos[0] + self.env.facing_direction[0],
                             self.env.agent_pos[1] + self.env.facing_direction[1]) and self.env.tiles[tile].passable:
+                    """
                     if self.tile_score(tile) < self.tile_score(self.env.agent_pos)-AGENT_AMBITION:
                         self.last_pos = self.env.agent_pos
                         self.env.perform_action(
@@ -56,28 +55,29 @@ class greedy(object):
                         else:
                             self.visited[self.env.agent_pos] = 1
                     else:
-                        self.options.append((self.tile_score(tile), tile))
+                    """
+                    self.options.append((self.tile_score(tile), tile))
                     break
-            if not acted:
-                self.env.perform_action(TURN_RIGHT, self)
+            #if not acted:
+            self.env.perform_action(TURN_RIGHT, self)
             i += 1
-        if not acted:
-            best_option = self.calc_best_option()
-            self.last_pos = self.env.agent_pos
-            self.env.perform_action(
-                (best_option[0]-self.env.agent_pos[0], best_option[1]-self.env.agent_pos[1]), self)
-            if self.env.agent_pos in self.visited.keys():
-                self.visited[self.env.agent_pos] += 1
-            else:
-                self.visited[self.env.agent_pos] = 1
+        #if not acted:
+        best_option = self.calc_best_option()
+        #self.last_pos = self.env.agent_pos
+        self.env.perform_action(
+            (best_option[0]-self.env.agent_pos[0], best_option[1]-self.env.agent_pos[1]), self)
+        if self.env.agent_pos in self.visited.keys():
+            self.visited[self.env.agent_pos] += 1
+        else:
+            self.visited[self.env.agent_pos] = 1
         self.options = []
 
     def tile_score(self, tile):
         visited_score = 0
         if tile in self.visited.keys():
             visited_score = self.visited[tile]
-        if tile == self.last_pos:
-            visited_score += BACKSTEP_PENALTY
+        #if tile == self.last_pos:
+        #    visited_score += BACKSTEP_PENALTY
         distance_score = math.sqrt((tile[0]-self.env.target[0])**2)/self.env.size[0] + \
             math.sqrt((tile[1]-self.env.target[1])**2)/self.env.size[1]
         return visited_score + distance_score
